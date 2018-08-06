37aa619b1eef0d62cfee1c0a7519ad26eb4c153e
ModyPadr <36020255+ModyPadr@users.noreply.github.com>
2018-08-06 13:15:43 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,239 @@
+//======================================[Const]======================================
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const bot = new Discord.Client();
+const ms = require("ms");
+const fs = require('fs');
+const moment = require('moment');
+const request = require('request');
+const dateFormat = require('dateformat');
+const r1 = require('snekfetch');
+const Canvas = require("canvas");
+const jimp = require('jimp')
+const weather = require('weather-js');
+const prefix = "$";
+const adminprefix = "$";
+const admin = "$";
+const developers = ["316324088865882142","408136927259131905","418064523157700609"]
+
+  
+
+
+
+//======================================[Client]======================================
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('guildMemberAdd', member=> {
+  var guild = '471426842667253761';
+    member.addRole(member.guild.roles.find("name","Clients"));
+    });
+    
+    client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var iiMo = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTimestamp()
+            .setTitle('``` New Dm Mesage ```')
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+            .setFooter(`From : (@${message.author.tag})  |  (${message.author.id})`)
+        client.channels.get("475889317907136513").send({ embed: iiMo });
+    }
+});
+    
+//======================================[Owners]======================================
+
+
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(admin + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`** :heart:  ${argresult}**`)
+  }  else  
+  if (message.content.startsWith(admin + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`** :heart:  ${argresult}**`)
+  } else 
+  if (message.content.startsWith(admin + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`** :heart:  ${argresult}**`)
+  } else 
+  if (message.content.startsWith(admin + 'stream')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`** :heart:  **`)
+  }
+  if (message.content.startsWith(admin + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(admin + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});  
+
+
+//======================================[ Log ]======================================
+
+
+client.on('message', message => {
+    if(!message.channel.guild) return;
+if (message.content.startsWith(prefix + 'ping')) {
+if(!message.channel.guild) return;
+var msg = `${Date.now() - message.createdTimestamp}`
+var api = `${Math.round(client.ping)}`
+if (message.author.bot) return;
+let embed = new Discord.RichEmbed()
+.setColor('#36393e')
+.addField(' Time Taken : ',msg + " ms")
+.addField(' WebSocket : ',api + " ms")
+message.channel.send({embed:embed});
+}
+});
+
+
+client.on('message', message => {
+  if(message.content.startsWith(prefix + "صراحة")) {
+    let mention = message.mentions.users.first();
+    let args = message.content.split(' ').slice(2).join(' ');
+    let filter = s => s.author.id === message.author.id;
+     var msg;
+     if(!mention) return message.channel.send(':x:| **منشن الشخص المطلوب ارسال الرسالة له**');
+    if(mention === client.user) return message.channel.send(':x:| **لا يمكنني ارسال رسالة لنفسي**');
+    if(mention.bot) return message.channel.send(':x:| **لا يمكنك ارسال الرسالة لبوت**');
+    if(mention === message.author) return message.channel.send(':x:| **لا يمكنك مصارحة نفسك**').then(m => { m.delete(3500)});
+    if(mention && !args) {
+      message.delete();
+      message.channel.send(`⚒| **\`عملية اعادة البناء\` انت لم تقم بكتابة الرسالة  اكتبها الان .. امامك 30 ثانية**`).then(m => {
+        message.channel.awaitMessages(filter, {
+          max: 1,
+          time: 30000,
+          errors: ['time']
+        }).then(collected => {
+          msg = collected.first().content;
+          collected.first().delete();
+          try {
+            m.edit(':white_check_mark:| **تم ارسال الصراحة**');
+            mention.send(`${mention}, لقد تم مصارحتك\n» الرسالة : ${msg}\n» التاريخ : ${moment().format('MMM Do YY')}`);
+          } catch(e) {
+            message.channel.send(':x:| **لم استطع ارسال الرسالة**');
+          }
+        });
+      });
+    } else if(mention && args) {
+      message.delete();
+      try {
+        message.channel.send(':white_check_mark:| **تم ارسال الصراحة**');
+        mention.send(`${mention}, لقد تم مصارحتك\n» الرسالة : ${args}\n» التاريخ : ${moment().format('MMM Do YY')}`);
+      } catch(e) {
+        console.log(e);
+        message.channel.send(':x:| **لم استطع ارسال الرسالة**');
+      }
+    }
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
