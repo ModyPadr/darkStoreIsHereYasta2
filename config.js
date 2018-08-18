32850022f45fd9d03a1cbaa42de9018f747d60bc
Rainbow <36020255+ModyPadr@users.noreply.github.com>
2018-08-18 21:36:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.PREFIX = '!';
 
-exports.TOKEN = 'NDczMzI5MzM0NDQzMDQ4OTcw.Dkt3zA.53J5adcMIILCtcJA3BHKUSJsGY4';
+exports.TOKEN = 'NDc4NTE0NDI5MzQ5MzMwOTUy.Dln_3g.2_mbV4d2oUxDDBvuR3PA07HZlC8';
 
 exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
